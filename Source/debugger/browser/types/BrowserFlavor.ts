f8c873b6b3dc5ad1615f54b72fabe7f43c5f920a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 02:07:24 +0300

    squash!

--- Source/debugger/browser/types/BrowserFlavor.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-
-/**
- * The PCF debugger will try to open the Microsoft Edge flavors in the following order: Stable, Beta, Dev and Canary.
- */
-export type BrowserFlavor = "Default" | "Stable" | "Beta" | "Dev" | "Canary";
